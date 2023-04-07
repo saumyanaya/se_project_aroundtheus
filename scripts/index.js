72ac1f14ab79ae86af33a024e2ca48bce4db9e2b
saumyanaya <126693951+saumyanaya@users.noreply.github.com>
2023-04-07 00:28:39 -0400

    S3P4

--- scripts/index.js
+++ scripts/index.js
@@ -24,4 +24,13 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
-console.log(initialCards);
+const profileEditButton = document.querySelector("#profile__edit-button");
+const profileEditModal = document.querySelector("#profile__edit-modal");
+const profileCloseButton = document.querySelector("#profile__close-button");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+profileCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
