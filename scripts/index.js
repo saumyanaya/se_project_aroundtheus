8fb23b2535a3a8c3d098cf4698acd0ca3793e91f
saumyanaya <126693951+saumyanaya@users.noreply.github.com>
2023-04-10 21:31:53 -0400

    Stage 4 Project 4 submission.

--- scripts/index.js
+++ scripts/index.js
@@ -27,10 +27,39 @@ const initialCards = [
 const profileEditButton = document.querySelector("#profile__edit-button");
 const profileEditModal = document.querySelector("#profile__edit-modal");
 const profileCloseButton = document.querySelector("#profile__close-button");
+const profileEditForm = document.querySelector("#profile__edit-form");
+const profileTitleInput = document.querySelector("#profile__title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile__description-input"
+);
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
 
-profileEditButton.addEventListener("click", () => {
+profileEditButton.addEventListener("click", function () {
   profileEditModal.classList.add("modal_opened");
 });
-profileCloseButton.addEventListener("click", () => {
+profileCloseButton.addEventListener("click", function () {
   profileEditModal.classList.remove("modal_opened");
 });
+profileEditForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  profileEditModal.classList.remove("modal_opened");
+});
+
+function getCardElement(data) {
+  let cardTemplate = document.querySelector("#card").content;
+  let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  let cardImage = cardElement.querySelector(".card__image");
+  let cardTitle = cardElement.querySelector(".card__title");
+  cardImage.setAttribute("src", data.link);
+  cardImage.setAttribute("alt", data.name);
+  cardElement.querySelector(".card__title").textContent = data.name;
+  return cardElement;
+}
+let cardList = document.querySelector(".cards__list");
+for (let cardData of initialCards) {
+  let cardElement = getCardElement(cardData);
+  cardList.append(cardElement);
+}
