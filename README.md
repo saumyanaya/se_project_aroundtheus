e34ba8248d8760e5bd7cceedb748fcea734e9950
saumyanaya <126693951+saumyanaya@users.noreply.github.com>
2023-04-24 19:27:53 -0400

    Code for Project 5

--- README.md
+++ README.md
@@ -1,27 +1,15 @@
-# Project 3: Around The U.S.
+# Project name
 
-### Overview
+Around The U.S.
 
-- Intro
-- Figma
-- Images
-- Github
+# Description/Functionality
 
-**Intro**
+A gallery of images from around the U.S. Converts to display on various mobile screen sizes.
 
-This project is made so all the elements are displayed correctly on popular screen sizes. I invested more time in completing this project, since it's more difficult than previous ones.
+# Technologies/Techniques
 
-**Figma**
+HTML/CSS combines with @media at-rules to create different layouts depending on the screen size viewing.
 
-- [Link to the project on Figma](https://www.figma.com/file/ii4xxsJ0ghevUOcssTlHZv/Sprint-3%3A-Around-the-US?node-id=0%3A1)
+# Github Pages
 
-**Link to github Project on github pages**
-
-- [Link to the project on Github](https://saumyanaya.github.io/se_project_aroundtheus/index.html)
-
-**Images**
-This link contains desktop view....tablet view....mobile view
-
-![desktop image](./demo/desktop__image.png)
-![tablet image](./demo/tablet__image.png)
-![mobo image](./demo/mobo__image.png)
+https://saumyanaya.github.io/se_project_aroundtheus/
--- scripts/index.js
+++ scripts/index.js
@@ -24,24 +24,40 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+const profileAddButton = document.querySelector("#profile__add-button");
 const profileEditButton = document.querySelector("#profile__edit-button");
 const profileEditModal = document.querySelector("#profile__edit-modal");
+const profileAddModal = document.querySelector("#profile__add-modal");
 const profileEditCloseButton = profileEditModal.querySelector(".modal__close");
 const profileEditForm = document.querySelector("#profile__edit-form");
+const profileAddForm = document.querySelector("#profile__add-form");
 const profileTitleInput = document.querySelector("#profile__title-input");
 const profileDescriptionInput = document.querySelector(
   "#profile__description-input"
 );
+
+const cardTemplate = document.querySelector("#card").content;
+const profileAddCloseButton = profileAddModal.querySelector(".modal__close");
 const profileTitle = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
-
+const cardTitleInput = document.querySelector("#card__title-input");
+const cardUrlInput = document.querySelector("#card__url-input");
+const cardUrl = document.querySelector(".card__url");
+const cardTitle = document.querySelector(".card__title");
 profileEditButton.addEventListener("click", function () {
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
   profileEditModal.classList.add("modal_opened");
 });
+profileAddButton.addEventListener("click", function () {
+  profileAddModal.classList.add("modal_opened");
+});
 profileEditCloseButton.addEventListener("click", function () {
   profileEditModal.classList.remove("modal_opened");
+});
+profileAddCloseButton.addEventListener("click", function () {
+  profileAddModal.classList.remove("modal_opened");
 });
 
 profileEditForm.addEventListener("submit", function (evt) {
@@ -50,18 +66,42 @@ profileEditForm.addEventListener("submit", function (evt) {
   profileDescription.textContent = profileDescriptionInput.value;
   profileEditModal.classList.remove("modal_opened");
 });
+profileAddForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  const name = cardTitleInput.value;
+  const link = cardUrlInput.value;
+  const cardElement = getCardElement({ name, link });
+  cardList.prepend(cardElement);
+  profileAddModal.classList.remove("modal_opened");
+});
+
 function getCardElement(data) {
-  const cardTemplate = document.querySelector("#card").content;
-  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardElement = cardTemplate.cloneNode(true);
   const cardImage = cardElement.querySelector(".card__image");
   const cardTitle = cardElement.querySelector(".card__title");
-  cardImage.setAttribute("src", data.link);
-  cardImage.setAttribute("alt", data.name);
+  const likeButton = cardElement.querySelector(".card__like-button");
+  likeButton.addEventListener("click", function () {
+    likeButton.classList.toggle("card__like-button_active");
+  });
+  const trashButton = cardElement.querySelector(".card__trash-button");
+  const cardList = cardElement.querySelector(".card");
+  trashButton.addEventListener("click", function () {
+    cardList.remove("card");
+  });
+
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
   cardTitle.textContent = data.name;
   return cardElement;
 }
 const cardList = document.querySelector(".cards__list");
-for (const cardData of initialCards) {
-  const cardElement = getCardElement(cardData);
+initialCards.forEach(function (card) {
+  const data = card;
+  const cardElement = getCardElement(data);
   cardList.append(cardElement);
-}
+});
+const cardOpenModal = document.querySelector("#card-open-modal");
+const cardImage = document.querySelector("#card__image");
+cardImage.addEventListener("click", function () {
+  cardOpenModal.classList.add("modal__image");
+});
